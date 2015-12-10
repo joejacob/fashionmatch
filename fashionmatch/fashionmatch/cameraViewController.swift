09c660010380ce21f710aaebb2a40854efa2af15
Joe Jacob <joejacob95@gmail.com>
2015-12-10 18:48:22 -0500

    starting to implement camera

--- fashionmatch/fashionmatch/cameraViewController.swift
+++ fashionmatch/fashionmatch/cameraViewController.swift
@@ -2,28 +2,59 @@
 //  cameraViewController.swift
 //  fashionmatch
 //
-//  Created by Joe Jacob on 9/8/15.
+//  Created by Joe Jacob on 12/10/15.
 //
 //
 
 import UIKit
+import AVFoundation
 
 class cameraViewController: UIViewController {
+
+    @IBOutlet weak var cameraView: UIImageView!
     
-    @IBOutlet weak var camView: UIImageView!
-    
-    @IBAction func takePhoto(sender: UIButton) {
+    @IBAction func takePhotoButton(sender: UIButton) {
+        
     }
+    
+    let captureSession = AVCaptureSession();
+    
+    // If we find a device, we'll store it here for later use
+    var captureDevice : AVCaptureDevice?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        captureSession.sessionPreset = AVCaptureSessionPresetLow
+        let devices = AVCaptureDevice.devices()
+        print(devices)
+        
+        // Loop through all the capture devices on this phone
+        for device in devices {
+            // Make sure this particular device supports video
+            if (device.hasMediaType(AVMediaTypeVideo)) {
+                // Finally check the position and confirm we've got the back
+                if (device.position == AVCaptureDevicePosition.Back) {
+                    captureDevice = device as? AVCaptureDevice
+                }
+            }
+        }
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
